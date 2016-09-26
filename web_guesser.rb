83dddd566a9382677a9f890031a8d92c142daa8a
Ryan Travitz <rtravitz@gmail.com>
2016-09-26 15:21:59 -0600

    Extension: Color changes working

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,20 +5,29 @@ SECRET_NUMBER = rand(100)
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  message, color = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 end
 
 def check_guess(guess)
-  if (guess - SECRET_NUMBER) >= 5
-    "Way too high!"
+  if guess.zero?
+    message = ""
+    color = "#fff"
+  elsif(guess - SECRET_NUMBER) >= 5
+    message = "Way too high!"
+    color = "red"
   elsif (guess - SECRET_NUMBER) <= -5
-    "Way too low!"
+    message = "Way too low!"
+    color = "red"
   elsif guess > SECRET_NUMBER
-    "Too high!"
+    message = "Too high!"
+    color = "pink"
   elsif guess < SECRET_NUMBER
-    "Too low!"
+    message = "Too low!"
+    color = "pink"
   else
-    "You got it right!\nThe secret number is #{SECRET_NUMBER}."
+    message = "You got it right!\nThe secret number is #{SECRET_NUMBER}."
+    color = "green"
   end
+  return message, color
 end
